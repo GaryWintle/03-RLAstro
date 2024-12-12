1ed6f349480f6f7ae88c5c9c85cc73ffc32e8d5c
ilovemodok <ilovemodok@hotmail.com>
2024-12-12 15:13:33 +0900

    trying something with gallery

---
+++ src/data/logos.js
@@ -0,0 +1,46 @@
+export const logos = [
+  {
+    src: "../assets/homepage/gallery/Best Botanical.svg",
+    alt: "Best Botanical",
+  },
+  {
+    src: "../assets/homepage/gallery/Burlington Cannabis.svg",
+    alt: "Burlington Cannabis",
+  },
+  {
+    src: "../assets/homepage/gallery/Canabis Culture.svg",
+    alt: "Canabis Culture",
+  },
+  { src: "../assets/homepage/gallery/Coca Cafe.svg", alt: "Coca Cafe" },
+  { src: "../assets/homepage/gallery/Cracked.svg", alt: "Cracked" },
+  { src: "../assets/homepage/gallery/fast 420.svg", alt: "Fast 420" },
+  {
+    src: "../assets/homepage/gallery/Fungi Finder.svg",
+    alt: "Fungi Finder",
+  },
+  {
+    src: "../assets/homepage/gallery/Guerra Consulting.svg",
+    alt: "Guerra Consulting",
+  },
+  { src: "../assets/homepage/gallery/GYDT.svg", alt: "GYDT" },
+  {
+    src: "../assets/homepage/gallery/harvest cannabis.svg",
+    alt: "Harvest Cannabis",
+  },
+  { src: "../assets/homepage/gallery/Highltimes.svg", alt: "Hightimes" },
+  { src: "../assets/homepage/gallery/MJ Creams.svg", alt: "MJ Creams" },
+  {
+    src: "../assets/homepage/gallery/Mushroom Dispensary.svg",
+    alt: "Mushroom Dispensary",
+  },
+  {
+    src: "../assets/homepage/gallery/overgrow canada.svg",
+    alt: "Overgrow Canada",
+  },
+  { src: "../assets/homepage/gallery/Pot TV.svg", alt: "Pot TV" },
+  {
+    src: "../assets/homepage/gallery/Pothead Books.svg",
+    alt: "Pothead Books",
+  },
+  { src: "../assets/homepage/gallery/Woodstock.svg", alt: "Woodstock" },
+];
