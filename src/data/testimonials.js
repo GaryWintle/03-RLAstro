e59f2ed3dc47d75556cd28e1bbf7b833f8b7599d
ilovemodok <ilovemodok@hotmail.com>
2024-12-15 12:10:35 +0900

    Added images to testimonials

---
+++ src/data/testimonials.js
@@ -0,0 +1,32 @@
+import jodiePic from "../assets/homepage/testimonials/jodie.jpg";
+import danaPic from "../assets/homepage/testimonials/dana.jpg";
+import coryPic from "../assets/homepage/testimonials/cory.jpg";
+import britneyPic from "../assets/homepage/testimonials/britney.jpg";
+
+export const testimonialData = [
+  {
+    name: "Jodie Emery",
+    company: "Cannabis Culture",
+    message:
+      "I’m so glad we got to make memories together! You’re a beloved core part of CC forever Gary!!",
+    profilePic: jodiePic,
+  },
+  {
+    name: "Dana Larsen",
+    company: "Pothead Books",
+    message: "The illustration is excellent! I love it.",
+    profilePic: danaPic,
+  },
+  {
+    name: "Cory McKay",
+    company: "The Surfin' Burrito",
+    message: "I love it. Jesus ur fast",
+    profilePic: coryPic,
+  },
+  {
+    name: "Britney Guerra",
+    company: "Guerra Consulting",
+    message: "Thanks so much. They are so perfect and so clean!!!",
+    profilePic: britneyPic,
+  },
+];
